bb86d955b9e095e902846929bda284202b11e417
SanjanaAbeykoon7 <sanjanaabeykoon1@gmail.com>
2025-11-09 17:58:52 +0530

    Initialize backend with Express and MongoDB integration

---
+++ backend/src/server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import noteRoutes from "./routes/notesRoutes.js";
+import { connectDB } from './config/db.js';
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5001
+
+connectDB();
+
+//middleware
+app.use(express.json())
+
+app.use("/api/notes", noteRoutes);
+
+app.listen(PORT, () => {
+    console.log("Server started on PORT:",PORT);
+});
